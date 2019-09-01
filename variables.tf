708c664009431faaae45f628d5f4d538df2b4886
Takayuki Kaburagi <t.kaburagi@me.com>
2019-09-01 14:21:21 +0900

    first commit

--- variables.tf
+++ variables.tf
@@ -1,23 +1,23 @@
 variable "vpc_id" {}
 
 variable "http_port" {
-  defaults = "8500"
+  default = "8500"
 }
 variable "https_port" {
-  defaults = "8501"
+  default = "8501"
 }
 variable "grpc_port" {
-  defaults = "8502"
+  default = "8502"
 }
 
 variable "cluster_rpc_port" {
-  defaults = "8300"
+  default = "8300"
 }
 variable "lan_serf" {
-  defaults = "8301"
+  default = "8301"
 }
 
 variable "cidr_blocks" {
-  type     = list(string)
-  defaults = ["0.0.0.0/0"]
+  type    = list(string)
+  default = ["0.0.0.0/0"]
 }
